b9060287fe5bb15beed895b80c71f7e769397a37
Clement Balestrat <clement.balestrat@gmail.com>
2020-08-28 16:39:23 +1000

    fix for Garth's typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,55 @@
-import Head from 'next/head'
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import Head from "next/head";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 
-import MainSection from '../sections/home/main';
-import BuildSection from '../sections/home/build';
-import TotalSection from '../sections/home/total';
-import EarnSection from '../sections/home/earn';
-import PartnersSection from '../sections/home/partners';
-import dynamic from 'next/dynamic'
-import { Layout } from '../components';
-import { fetchTotalLocked } from '../../lib/exchange-api';
+import MainSection from "../sections/home/main";
+import BuildSection from "../sections/home/build";
+import TotalSection from "../sections/home/total";
+import EarnSection from "../sections/home/earn";
+import PartnersSection from "../sections/home/partners";
+import dynamic from "next/dynamic";
+import { Layout } from "../components";
+import { fetchTotalLocked } from "../../lib/exchange-api";
 
 export interface ApiStatsProps {
-	totalLocked?: number
+	totalLocked?: number;
 }
 
-const PoweredBy = dynamic(
-  () => import('../sections/home/poweredBy'),
-  { ssr: false }
-)
+const PoweredBy = dynamic(() => import("../sections/home/poweredBy"), {
+	ssr: false,
+});
 
 const Home = ({ totalLocked }: ApiStatsProps) => {
-  return (
-    <>
-      <Head>
-        <title>Synthetix</title>
-        <link rel="icon" href="/favicon.ico" />
-				<meta name="description" content "Synthetix is a derivatives liquidity protocol providing the backbone for derivatives trading in DeFi, 
-						allowing anyone, anywhere to gain on-chain exposure to a vast range of assets.">
-				<meta name="og:image" content="/public/logo-x.png">
-      </Head>
-      <Layout>
+	return (
+		<>
+			<Head>
+				<title>Synthetix</title>
+				<link rel="icon" href="/favicon.ico" />
+				<meta
+					name="description"
+					content="Synthetix is a derivatives liquidity protocol providing the backbone for derivatives trading in DeFi, allowing anyone, anywhere to gain on-chain exposure to a vast range of assets."
+				/>
+				<meta name="og:image" content="/public/logo-x.png" />
+			</Head>
+			<Layout>
 				<MainSection />
 				<TotalSection totalLocked={totalLocked} />
 				<BuildSection />
 				<EarnSection />
 				<PoweredBy />
 				<PartnersSection />
-      </Layout>
-    </>
-  )
-}
+			</Layout>
+		</>
+	);
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const totalLocked = await fetchTotalLocked()
+	const totalLocked = await fetchTotalLocked();
 
-  return { props: {
-		totalLocked
-	 }}
-}
+	return {
+		props: {
+			totalLocked,
+		},
+	};
+};
 
-export default Home
+export default Home;
