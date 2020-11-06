0df9615cd5d90fbd683c6da815545c30aa5f1831
Jakub Mucha <jakub.mucha@icloud.com>
2020-11-06 18:37:05 +0100

    feat: add IP whitelisting
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
+import dynamic from 'next/dynamic';
+
+import ipRangeCheck from 'ip-range-check';
 
 import MainSection from '../sections/home/main';
 import BuildSection from '../sections/home/build';
 import TotalSection from '../sections/home/total';
 import EarnSection from '../sections/home/earn';
 import PartnersSection from '../sections/home/partners';
-import dynamic from 'next/dynamic';
 import { Layout } from '../components';
 import { fetchTotalLocked } from '../../lib/exchange-api';
 
@@ -45,13 +47,29 @@ const Home = ({ totalLocked }: ApiStatsProps) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const totalLocked = await fetchTotalLocked();
-
-	return {
-		props: {
-			totalLocked,
-		},
-	};
+	if (process.env.CF_IP) {
+		const allowedIps = JSON.parse(`[${process.env.CF_IP}]`);
+		const ip = context.req.headers['x-forwarded-for'] || context.req.connection.remoteAddress;
+		if (typeof ip === 'string' && ipRangeCheck(ip, allowedIps)) {
+			const props = await getProps();
+			return props;
+		} else {
+			context.res.statusCode = 403;
+			context.res.end('Your IP is not whitelisted.');
+			return {};
+		}
+	} else {
+		const props = await getProps();
+		return props;
+	}
+	async function getProps() {
+		const totalLocked = await fetchTotalLocked();
+		return {
+			props: {
+				totalLocked,
+			},
+		};
+	}
 };
 
 export default Home;
