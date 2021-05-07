1c78d59c4575ac801dde3b0997e29d4fa9ae9b41
AndyTCF <andy.c.f.98@gmail.com>
2021-05-07 13:20:23 +1000

    finalise synths section

--- src/sections/home/synths.tsx
+++ src/sections/home/synths.tsx
@@ -12,69 +12,100 @@ const SynthSection = () => (
 				Capture the price movements of popular cryptopcurrencies, fiat currencies, stocks,
 				commodities and more with zero slippage.
 				<br />
+				<br />
 				Synthetic assets or Synths, are assets voted into existence by the community which represent
 				the price of different types of assets.
 			</SectionDescription>
 			<BuildButton buttonType="secondary" link="/synths" external={false}>
-				Explore
+				Explore Synths
 			</BuildButton>
 		</Right>
-
 		<Line />
 	</SynthContainer>
 );
 
 const SynthContainer = styled(Section)`
-	min-height: 891px;
+	${(props) => props.theme.animations.show};
 
+	opacity: 0;
+	animation-delay: 400ms;
+	height: 100vh;
+	min-height: 753px;
 	display: flex;
-	justify-content: center;
 	align-items: center;
+	overflow: hidden;
 
-	${media.lessThan('large')`
-		min-height: 750px;
+	${media.lessThan('medium')`
 		flex-direction: column-reverse;
-		justify-content: center;
-		
+		height: 634px;
+		min-height: 634px;
+		margin: 80px 0px;
+		height: 100%;
+`}
+
+	${media.greaterThan('1350px' as any)`
+		padding: 0 0 0 153px;
 	`}
 `;
 
 const Right = styled.div`
+	max-width: 400px;
+	height: 364px;
 	position: absolute;
-	right: -100px;
-	width: 50%;
+	right: 0;
+	margin-right: 50px;
+	margin-top: -84px;
 
-	${media.lessThan('large')`
-		right: 140px;
-		max-width: 200px;
+	${media.greaterThan('1350px' as any)`
+		margin-right: 175px;
 	`}
 
 	${media.lessThan('medium')`
 		position: relative;
+		margin-left: auto;
+		margin-right: auto;
 		text-align: center;
-		right: 0px;
-		width: 100%;
-		margin: 48px 0px;
+		margin-top: 60px;
+
+		p {
+			text-align: center;
+			max-width: 100%;
+		}
+
+		br {
+			margin: 8px 0px;
+		}
 	`}
 `;
 
 const SynthGraphic = styled.img`
+	height: 444px;
 	position: absolute;
-	left: 0px;
-	height: auto;
-	width: 50%;
+	left: -250px;
+	pointer-events: none;
 
-	${media.lessThan('large')`
-		width: 65%;
-		right: 0;
+	${media.greaterThan('1350px' as any)`
+		height: 35vw;
+		left: -120px;
+		right: auto;
 	`}
 
 	${media.lessThan('medium')`
 		position: relative;
-		width: 100%;
-		right: auto;
-		margin: 48px 0px;
+		width: 1000px;
+		height: auto;
+		margin: auto;
+		margin-top: 26px;
+		margin-bottom: 42px;
+	`}
 
+	${media.lessThan('small')`
+		position: relative;
+		width: 650px;
+		height: auto;
+		margin: auto;
+		margin-top: 42px;
+		margin-bottom: 64px;
 	`}
 `;
 
