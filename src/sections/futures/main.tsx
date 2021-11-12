ce4cf1bd13d4335f58f006e20a477faf9fff8912
fritzschoff <max.j.f@gmx.de>
2021-11-12 09:57:54 +0100

    added big screen picture

--- src/sections/futures/main.tsx
+++ src/sections/futures/main.tsx
@@ -7,34 +7,41 @@ import media from 'styled-media-query';
 
 export default function FuturesMain() {
 	return (
-		<FuturesMainSectionTitle>
-			<SectionTitle>
-				Decentralized <br />
-				Perpetual <br />
-				Futures
-			</SectionTitle>
-			<FuturesMainSubline>
-				Many platforms already leverage the deep liquidity and composability of Synthetix to deliver
-				better trades with lower slippage, hedging, and other unique use cases.&nbsp;
-				<b>Synthetix perpetual futures are available exclusively through Kwenta.</b>
-			</FuturesMainSubline>
-			<FlexDiv>
-				<BuildButton link="https://kwenta.io">Start Trading</BuildButton>
-				<BuildButton link="https://discord.com/invite/AEdUHzt">Discord</BuildButton>
-			</FlexDiv>
-			<KwentaScreen src="/home/futures/kwenta-screen.png" />
-			{/* <Right /> */}
+		<FuturesMainSection>
+			<FuturesContentWrapper>
+				<SectionTitle>
+					Decentralized <br />
+					Perpetual <br />
+					Futures
+				</SectionTitle>
+				<FuturesMainSubline>
+					Many platforms already leverage the deep liquidity and composability of Synthetix to
+					deliver better trades with lower slippage, hedging, and other unique use cases.&nbsp;
+					<b>Synthetix perpetual futures are available exclusively through Kwenta.</b>
+				</FuturesMainSubline>
+				<FlexDiv>
+					<BuildButton link="https://kwenta.io">Start Trading</BuildButton>
+					<BuildButton link="https://discord.com/invite/AEdUHzt">Discord</BuildButton>
+				</FlexDiv>
+			</FuturesContentWrapper>
+			<KwentaScreen />
 			<Gradient />
-		</FuturesMainSectionTitle>
+		</FuturesMainSection>
 	);
 }
 
-const FuturesMainSectionTitle = styled(Section)`
+const FuturesMainSection = styled(Section)`
 	padding: 0 0 0 138px;
 	height: calc(100vh - ${headerHeight}px);
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	overflow-x: visible;
+`;
+
+const FuturesContentWrapper = styled.div`
+	position: absolute;
+	z-index: 10;
 `;
 
 const FuturesMainSubline = styled(Subline)`
@@ -50,9 +57,25 @@ const BuildButton = styled(Button)`
 
 const KwentaScreen = styled.img`
 	position: absolute;
-	right: 0;
+	top: 200px;
+	right: -50px;
 	width: 713px;
 	height: auto;
+	content: url('/home/futures/kwenta-screen.png');
+
+	${media.greaterThan('huge')`
+		content: url("/home/futures/kwenta-screen-big.png");
+		right:8px;
+		width: 900px;
+		height: auto;
+	`}
+
+	${media.greaterThan('1900px' as any)`
+		right: -50px;
+		width: auto;
+		height: 850px;
+		content: url("/home/futures/kwenta-screen-big.png")
+		`}
 `;
 
 const Gradient = styled.div`
@@ -62,8 +85,16 @@ const Gradient = styled.div`
 	width: 100%;
 	height: 100vh;
 	min-height: 853px;
-	background: linear-gradient(rgba(14, 5, 47, 0) 60%, #0e052f 80%);
+	background: linear-gradient(rgba(14, 5, 47, 0) 40%, #0e052f 65%);
 	pointer-events: none;
+
+	${media.greaterThan('huge')`
+		background: linear-gradient(rgba(14, 5, 47, 0) 40%, #0e052f 80%);
+	`}
+
+	${media.greaterThan('1900px' as any)`
+		background: linear-gradient(rgba(14, 5, 47, 0) 40%, #0e052f 90%);
+	`}
 
 	${media.lessThan('medium')`
 		height: 734px;
