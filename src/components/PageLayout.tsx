da7ddf63628322e5920bb7f9138693a1e6e0400d
fritzschoff <max.j.f@gmx.de>
2021-12-02 17:28:57 +0100

    removed unused comp prop

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 import { FlexDivColCentered } from 'src/styles/common';
 import { Footer, Header } from '.';
 
-export default function PageLayout({ children, backgroundColor }: PropsWithChildren<{}>) {
+export default function PageLayout({ children }: PropsWithChildren<{}>) {
 	return (
 		<FlexDivColCentered>
 			<Header />
