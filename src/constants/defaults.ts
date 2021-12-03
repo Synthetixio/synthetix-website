9c6a2b814ff65069124b8b18746f63e1a74fcf75
fritzschoff <max.j.f@gmx.de>
2021-12-03 16:47:23 +0100

    removed default network

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,8 +1,3 @@
-import { NetworkId } from '@synthetixio/contracts-interface';
-
-// network defaults
-export const DEFAULT_NETWORK_ID = NetworkId.Mainnet;
-
 export const DEFAULT_FIAT_DECIMALS = 2;
 export const DEFAULT_NUMBER_DECIMALS = 2;
 export const DEFAULT_TOKEN_DECIMALS = 18;
