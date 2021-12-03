104409c6cb1d7adb6139c21d81a5e5bf4d52cf78
fritzschoff <max.j.f@gmx.de>
2021-12-03 16:24:34 +0100

    removed unused CI actions

--- src/lib/snxjs.ts
+++ src/lib/snxjs.ts
@@ -8,6 +8,7 @@ let snxjs: SynthetixJS;
 
 const getSNXJS = (config?: Config) => {
 	if (config || !snxjs) {
+		console.log(config);
 		snxjs = initSnxJS({
 			networkId: DEFAULT_NETWORK_ID,
 			// @ts-ignore TODO #152
