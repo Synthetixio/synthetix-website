1b5cc433d2355d9a7e4b5dc41b4106f86644048c
Peiman <peymannaderi3@yahoo.com>
2022-04-13 11:55:39 +0430

    fix: update synths test

--- tests/e2e/specs/synths-page.spec.js
+++ tests/e2e/specs/synths-page.spec.js
@@ -1,6 +1,6 @@
 describe('Synths Page', () => {
 	it.only('should have at least 3 tabs', () => {
 		cy.visit('http://localhost:3000/synths');
-		cy.get('button[data-test-id="tab-categories"]').should('have.length.gt', 3);
+		cy.get('button[data-test-id="tab-categories"]').should('have.length.eq', 2);
 	});
 });
