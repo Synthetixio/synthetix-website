5e3b219205543e13465da582272e890a1671c906
fritzschoff <fritzschoff@icloud.com>
2022-09-02 16:19:46 +0100

    adding chakra

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx: any) {
 		const sheet = new ServerStyleSheet();
