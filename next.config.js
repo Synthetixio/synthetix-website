1c19af64ed7e45d974ce02d278e2661f6ff0cc92
fritzschoff <fritzschoff@icloud.com>
2022-09-05 15:16:13 +0100

    updating versions

--- next.config.js
+++ next.config.js
@@ -30,34 +30,4 @@ module.exports = {
 		domains: ['cdn.sanity.io'],
 	},
 	productionBrowserSourceMaps: true,
-	webpack5: true,
-	webpack: (config, { isServer, buildId }) => {
-		if (isServer) {
-			// IS_SERVER_INITIAL_BUILD is meant to be defined only at build time and not at run time, and therefore must not be "made public"
-			process.env.IS_SERVER_INITIAL_BUILD = '1';
-		}
-
-		const APP_VERSION_RELEASE = `${packageJson.version}_${buildId}`;
-		config.plugins.map((plugin) => {
-			if (plugin.definitions) {
-				// If it has a "definitions" key, then we consider it's the DefinePlugin where ENV vars are stored
-				// Dynamically add some "public env" variables that will be replaced during the build through "DefinePlugin"
-				// Those variables are considered public because they are available at build time and at run time (they'll be replaced during initial build, by their value)
-				plugin.definitions['process.env.NEXT_PUBLIC_APP_BUILD_ID'] = JSON.stringify(buildId);
-				plugin.definitions['process.env.NEXT_PUBLIC_APP_VERSION_RELEASE'] =
-					JSON.stringify(APP_VERSION_RELEASE);
-			}
-		});
-
-		if (isServer) {
-			// Trick to only log once
-			console.debug(
-				`[webpack] Building release "${APP_VERSION_RELEASE}" using NODE_ENV="${
-					process.env.NODE_ENV
-				}" ${process.env.IS_SERVER_INITIAL_BUILD ? 'with IS_SERVER_INITIAL_BUILD="1"' : ''}`
-			);
-		}
-
-		return config;
-	},
 };
