7f900ca88a6ac2fc6ade4bccae28b088d2626bcf
fritzschoff <fritzschoff@icloud.com>
2024-02-06 14:54:06 +0000

    ini

--- pages/governance.tsx
+++ pages/governance.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import { PageLayout } from 'src/components';
-import FurtherLinking from 'src/sections/governance/furtherLinking';
 import GovernanceMain from 'src/sections/governance/main';
-import Table from 'src/sections/governance/table';
-import { Line } from 'src/styles/common';
 
 export default function Governance() {
 	return (
@@ -13,9 +10,6 @@ export default function Governance() {
 			</Head>
 			<PageLayout>
 				<GovernanceMain />
-				<Table />
-				<FurtherLinking />
-				<Line showOnMobile />
 			</PageLayout>
 		</>
 	);
