158382529f813a049b28d733c47dd76da36d5d64
howitsjade <jade@cc.snxdao.io>
2024-10-22 10:43:05 +1000

    update copy and section

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -5,5 +5,9 @@ export const links = {
 	liquidityApp: 'https://liquidity.synthetix.eth.limo/',
 	stakingGuide: 'https://docs.synthetix.io/staking/staking-guide',
 	v3IntegrationGuide: 'https://docs.synthetix.io/v/v3/',
-	governanceApp: 'https://governance.synthetix.io/ff',
+	governanceApp: 'https://governance.synthetix.io/',
+	governanceDocs:
+		'https://docs.synthetix.io/synthetix-v3-user-documentation/dao-and-governance/governance',
+	vision:
+		'https://mirror.xyz/0xF0Ab6151b0705790378b4c3504918a85c5E39246/DEuFmBrXyjaxe7CFFhzrotdyZFsG9xnFpLd2t3TFAro',
 };
